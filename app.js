de8e015f5cb14420ad308d7d8634c9d7ec49bbe8
jackpotnick <jackpotnick777@gmail.com>
2015-11-13 11:27:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6Lfk3BATAAAAABSljSKDiIrQzM4WEYCM3r6m3YfH',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  redirect_uri: 'https://jackpotnick.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
